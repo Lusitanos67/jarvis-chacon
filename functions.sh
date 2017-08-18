6fb02741bf49f5a427c3e8f02f4c9e887e5b8d14
Lusitanos67 <Lusitanos67@hotmail.com>
2017-08-18 10:03:04 +0200

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -1,20 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Turn chacon device on/off
+
 pg_chacon_turn () {
     # $1: action [on|off]
     # $2: received order
     local -r order="$(jv_sanitize "$2")"
-    while read device; do
-        local sdevice="$(jv_sanitize "$device" ".*")"
-        if [[ "$order" =~ .*$sdevice.* ]]; then
-            local address="$(echo $pg_chacon_config | jq -r ".devices[] | select(.name==\"$device\") | .address")"
-            say "$(pg_chacon_lg "switching_$1" "$2")"
-            local cmd="sudo ./$pg_chacon_dir/chacon_send 0 $pg_chacon_num $address $1"
-            $verbose && jv_debug "$> $cmd"
-            eval $cmd | while read line; do jv_debug "$line"; done # safe as no user input in $cmd. pass each output line to jv_debu
-			say "c'est fait"
-            return $?
-        fi
-    done <<< "$(echo $pg_chacon_config | jq -r '.devices[].name')"
+  while read device; do
+    local sdevice="$(jv_sanitize "$device" ".*")"
+    if [[ "$order" =~ .*$sdevice.* ]]; then
+      local address="$(echo $pg_chacon_config | jq -r ".devices[] | select(.name==\"$device\") | .address")"
+      local dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+        say "$(pg_chacon_lg "switching_$1" "$2")"
+      local cmd="sudo $dir/chacon_send $pg_chacon_gpio_pin $pg_chacon_num $address $1"
+	$verbose && jv_debug "$> $cmd"
+      eval $cmd | while read line; do jv_debug "$line"; done
+        say "$(pg_chacon_lg "switching_done")"
+      return $?
+    fi
+  done <<< "$(echo $pg_chacon_config | jq -r '.devices[].name')"
     say "$(pg_chacon_lg "no_device_matching" "$2")"
-    return 1
+  return 1
 }
