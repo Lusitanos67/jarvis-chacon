03cf4a664464327dd3a4adccbc1ce91aa0d46816
Lusitanos67 <Lusitanos67@hotmail.com>
2017-08-18 10:03:31 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,5 @@
 pg_chacon_gpio_pin=0 # GPIO PIN number (don't touch if you followed the README explanation)
 pg_chacon_num=12345678 # 12345678 for DIO 54760 remote control, 12325261 for other DIO remote control (or any other number if you don't use the remote controler)
-pg_chacon_dir=plugins/jarvis-chacon
 pg_chacon_config='{ "devices":[
     { "name": "SALON", "address": "1"},
     { "name": "SALLE A MANGER", "address": "2"},
