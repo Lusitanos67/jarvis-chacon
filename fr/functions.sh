50d8c42bfb176084443af3a14b59652c2a40471d
Lusitanos67 <Lusitanos67@hotmail.com>
2017-08-18 09:54:20 +0200

    Update functions.sh

--- fr/functions.sh
+++ fr/functions.sh
@@ -3,7 +3,7 @@ pg_chacon_lg() {
         no_device_matching) echo "Aucun appareil ne correspond à $2";;
         switching_on) echo "J'allume $2";;
         switching_off) echo "J'éteins $2";;
-        switching_off_done) echo "c'est fait";;
+        switching_done) echo "c'est fait";;
     *) jv_error "Erreur: Aie, je ne reconnais pas '$2'";;
     esac
 }
